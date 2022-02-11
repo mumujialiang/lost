1703313bedc2ac0954cbb610b670f53a8e224399
林嘉亮 <ljialiang21@gmail.com>
2022-02-12 01:16:00 +0800

    chore: 增加markdown作为vue组件的支持

--- src/views/test/TestView.vue
+++ src/views/test/TestView.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
 import carbonWarning from '~icons/carbon/warning'
+
+import ComMD from './test.md'
 </script>
 
 <template>
   <div>new component</div>
   <carbon-warning />
+  <ComMD />
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,20 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Inspect from 'vite-plugin-inspect'
 import Icons from 'unplugin-icons/vite'
+import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
     vueJsx(),
     Inspect(),
     Icons({
       autoInstall: true
-    })
+    }),
+    Markdown()
   ],
   resolve: {
     alias: {
