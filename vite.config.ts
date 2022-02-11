b4568594a26f50543fdec811a0783741545d7ccd
林嘉亮 <ljialiang21@gmail.com>
2022-02-11 23:11:08 +0800

    chore: 增加可查看页面依赖拓扑图

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), Inspect()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
