e34d351f68ad48c0285a3946ac280a7ec6c3cf8a
林嘉亮 <ljialiang21@gmail.com>
2022-02-20 12:47:04 +0800

    feat: 增加页面标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+  }
+}
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/test',
+      meta: {
+        title: '测试'
+      },
       component: () => import('../views/test/TestView.vue')
     }
   ]
+})
+
+router.beforeEach(to => {
+  window.document.title = to.meta.title || '标题'
 })
 
 export default router
