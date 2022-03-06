d111754d5a74adae95578fb39cde40b5f89838ed
林嘉亮 <ljialiang21@gmail.com>
2022-03-06 15:14:48 +0800

    feat: 增加进度步骤器

--- src/views/user/components/contact/components/step/ComStep.vue
+++ src/views/user/components/contact/components/step/ComStep.vue
@@ -1,4 +1,9 @@
 <script lang="ts" setup>
+import { AlarmClock, Upload, Phone } from '@element-plus/icons-vue'
+import ComUpload from './components/upload/ComUpload.vue'
+import ComWait from './components/wait/ComWait.vue'
+import ComFinish from './components/finish/ComFinish.vue'
+
 const props = defineProps<{
   modelValue: boolean
   loading: boolean
@@ -7,6 +12,10 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'update:model-value', value: boolean): void
 }>()
+
+const getComponentName = () => {
+  return ComUpload
+}
 </script>
 
 <template>
@@ -15,7 +24,16 @@ const emit = defineEmits<{
     title="当前进度"
     width="800px"
     @update:model-value="emit('update:model-value', $event)"
-  ></el-dialog>
+  >
+    <el-steps :space="200" :active="0" simple>
+      <el-step title="上传备案资料" :icon="Upload"></el-step>
+      <el-step title="等待人工审核" :icon="AlarmClock"></el-step>
+      <el-step title="获得联系电话" :icon="Phone"></el-step>
+    </el-steps>
+    <div class="container">
+      <component :is="getComponentName()"></component>
+    </div>
+  </el-dialog>
 </template>
 
 <style lang="scss" scoped></style>
